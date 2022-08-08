61abfac3285f7c8664f46a0cd5604d234e9287d9
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2022-08-08 23:08:13 +0900

    init

---
+++ src/test/kotlin/com/example/emoji/EmojiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.emoji
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmojiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
